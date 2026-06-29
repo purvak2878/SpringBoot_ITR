345e987e85f915450dbda1b465f9c5936c5aca6d
purvak2878 <purvakulkarni456@gmail.com>
2026-06-29 15:17:41 +0530

    week 5 Tasks

---
+++ sample/src/main/java/com/itr/sample/common/BaseballCoach.java
@@ -0,0 +1,19 @@
+package com.itr.sample.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Primary;
+
+@Lazy
+@Component
+public class BaseballCoach implements Coach {
+
+    public BaseballCoach() {
+        System.out.println("In constructor: BaseballCoach");
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes on batting practice.";
+    }
+}
---
+++ sample/src/main/java/com/itr/sample/common/Coach.java
@@ -0,0 +1,5 @@
+package com.itr.sample.common;
+
+public interface Coach {
+    String getDailyWorkout();
+}
